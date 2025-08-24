ce1cdbd5987ff6757de6bb165d82f38c5279ff07
Prashant Patil <patilanilprashant178@gmail.com>
2025-08-24 13:56:33 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true, // 🚀 disables ESLint during build
   },
   reactStrictMode: true,
-  swcMinify: false, // Disable SWC minification
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && isServer) {
-      // Disable minification for server-side bundles
-      config.optimization.minimize = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
